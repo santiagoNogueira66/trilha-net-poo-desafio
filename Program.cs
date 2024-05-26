ded8b56317515ade1c753a72d38476798e5a3f4a
santiago <santiagonogueira66@gmail.com>
2024-05-26 20:08:50 -0300

    projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("(85) 98424-7755", "nokia tijolão","123456", 128);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("whatssap");
+nokia.ReceberLigacao();
